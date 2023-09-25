bfe2102c5aef6bf262cfa30875567f08bc3d5990
Palak Bansal <palakbansal3233@gmail.com>
2023-09-25 17:23:37 +0530

    Setting up store

---
+++ src/services/article.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const articleApi = createApi({
+  reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "a0954503ccmsh757dc1cc15dc48ep165b02jsnc4269e5db7cf"
+      );
+      headers.set(
+        "X-RapidAPI-Host",
+        "article-extractor-and-summarizer.p.rapidapi.com"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getSummary: builder.query({
+      query: (params) =>
+        `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+    }),
+  }),
+});
+
+export const { useLazyGetSummaryQuery } = articleApi;
