99042028dbceb30322cc520a05bf9b82b5013db6
Palak Bansal <palakbansal3233@gmail.com>
2023-09-25 15:14:29 +0530

    Complete Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,27 @@ import { logo } from "../assets";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt="logo" className="w-28 object-contain" />
         <button
           type="button"
           onClick={() =>
             window.open("https://github.com/palakbansal3233/summarize")
           }
-        />
+          className="black_btn"
+        >
+          Github
+        </button>
       </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT - 4</span>
+      </h1>
+      <h2 className="desc">
+        Revolutionize Your Reading: Discover an Open-Source Article Summarizer
+        That Condenses Lengthy Articles into Digestible Gems of Knowledge!
+      </h2>
     </header>
   );
 };
